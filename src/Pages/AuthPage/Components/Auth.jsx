3632cd2a1eb6dca79e5420a762edc71a6a0f49c0
PIut0 <slimdoks@gmail.com>
2022-01-30 00:09:16 +0900

    Feat: 인증하기 페이지 스타일 적용

--- src/Pages/AuthPage/Components/Auth.jsx
+++ src/Pages/AuthPage/Components/Auth.jsx
@@ -4,10 +4,11 @@ import FtAuthService from '../../../Network/FtAuthService';
 import { LoadingButton } from '@mui/lab';
 import { TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import GlobalStyled from '../../../Styled/Global.styled';
 
 const authRequestInformation = intraId => {
   return (
-    <>
+    <div className="send_info">
       <div>
         <h2>{intraId}@student.42seoul.kr</h2>
         <h3>으로 인증메일이 전송되었습니다.</h3>
@@ -16,23 +17,19 @@ const authRequestInformation = intraId => {
         <h3>받으신 이메일을 열어 ‘인증하기’ </h3>
         <h3>버튼을 클릭하면 인증이 완료됩니다.</h3>
       </div>
-    </>
+    </div>
   );
 };
 
 const authRequestCheckStep = handleSendReset => {
   return (
-    <>
-      <div>
-        <h4>혹시 메일을 받지 못하셨나요?</h4>
-        <h4>전송에 최대 5분이 소요될 수 있습니다.</h4>
-        <h4>스팸편지함을 확인해주세요.</h4>
-        <h4>인트라 아이디에 오타가 없는지 확인해주세요.</h4>
-        <button onClick={handleSendReset}>
-          인트라 아이디에 오타가 없는지 확인해주세요.
-        </button>
-      </div>
-    </>
+    <div className="error_info">
+      <h4>혹시 메일을 받지 못하셨나요?</h4>
+      <h4>1. 전송에 최대 5분이 소요될 수 있습니다.</h4>
+      <h4>2. 스팸편지함을 확인해주세요.</h4>
+      <h4>3. 인트라 아이디에 오타가 없는지 확인해주세요.</h4>
+      <button onClick={handleSendReset}>인트라 아이디 다시 입력하기</button>
+    </div>
   );
 };
 
@@ -77,16 +74,18 @@ const Auth = () => {
   return (
     <Styled.AuthDiv>
       <div className="input_div">
-        <TextField
+        <input
           name="email"
           value={input.email}
           onChange={onChange}
-          id="standard-basic"
           label="인트라 아이디"
+          type="text"
+          placeholder="Intra ID"
         />
-        <h1></h1>
+        <h5 className="domain"></h5>
       </div>
       <LoadingButton
+        className="send_button"
         onClick={handleAuthenticate}
         endIcon={<SendIcon />}
         loading={isBlock}
